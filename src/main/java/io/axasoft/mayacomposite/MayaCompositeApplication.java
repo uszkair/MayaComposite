71d7642aea0db18e7f2bb12940835762d19d1678
RóbertUszkai <robert.uszkai@r34dy.io>
2024-11-06 22:20:08 +0100

    init

---
+++ src/main/java/io/axasoft/mayacomposite/MayaCompositeApplication.java
@@ -0,0 +1,13 @@
+package io.axasoft.mayacomposite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayaCompositeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MayaCompositeApplication.class, args);
+    }
+
+}
